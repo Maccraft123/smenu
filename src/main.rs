edcfcdb62606073b4326909228036deb3b5503d5
Maccraft123 <maccraft123mc@gmail.com>
2022-12-18 23:55:27 +0100

    add initial rom handling

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,18 @@ use serde::{Serialize, Deserialize};
 use anyhow::{Result, Context};
 use std::{
     process::{Command, Stdio},
-    path::PathBuf,
-    fs::{File, OpenOptions},
+    fs::{self, File, OpenOptions},
     thread,
     io::{
         Read, BufReader, BufRead,
         Write,
     },
+    path::{Path, PathBuf},
     os::unix::{
         io::AsRawFd,
         process::ExitStatusExt,
     },
+    collections::{HashSet, HashMap},
 };
 
 use libdogd::{log_debug, log_info, log_error, log_critical, LogPriority, post_log, log_rust_error};
@@ -29,7 +30,6 @@ use libdogd::{log_debug, log_info, log_error, log_critical, LogPriority, post_lo
 enum Category {
     Tools,
     Programs,
-    Emulators,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -45,38 +45,118 @@ struct MenuEntry {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+struct Emulator {
+    executable: PathBuf,
+    #[serde(default)]
+    args: Vec<String>,
+    #[serde(default)]
+    env: Vec<(String, String)>,
+    systems: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct System {
+    name: String,
+    rom_directory: PathBuf,
+    file_extensions: HashSet<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 struct MenuLayout {
     #[serde(rename = "item")]
     items: Vec<MenuEntry>,
+    #[serde(rename = "emulator")]
+    emulators: Vec<Emulator>,
+    #[serde(rename = "system")]
+    systems: Vec<System>,
 }
 
 impl MenuLayout {
-    fn mk_sgui_layout(&self) -> Layout {
+    fn mk_sgui_layout(self) -> (HashMap<u128, MenuEntry>, Layout) {
+        let mut id = 0;
+        let mut entry_map = HashMap::new();
         let mut tools = Vec::new();
         let mut programs = Vec::new();
+        let mut roms = Vec::new();
 
-        for (id, item) in self.items.iter().enumerate() {
+        for item in self.items.into_iter() {
             match item.category {
-                Category::Tools => tools.push((&item.name, id )),
-                Category::Programs => programs.push((&item.name, id)),
-                Category::Emulators => todo!("emulators are not supported yet"),
-                //Category::Emulators => emulators.insert((&item.name, id))
+                Category::Tools => tools.push((item, id)),
+                Category::Programs => programs.push((item, id)),
+            }
+            id += 1;
+        }
+
+        for system in self.systems.iter() {
+            if system.rom_directory.exists() {
+                let files = match fs::read_dir(&system.rom_directory) {
+                    Ok(f) => f,
+                    Err(e) => {
+                        log_rust_error(&e, format!("Failed to open rom directory for {}", &system.name), LogPriority::Error);
+                        continue;
+                    }
+                };
+                let Some(emulator) = self.emulators.iter().find(|e| e.systems.contains(&system.name)) else {
+                    log_error(format!("Failed to find suitable emulator for system {}", &system.name));
+                    continue;
+                };
+
+                let mut system_tab = Vec::new();
+                for file in files {
+                    let Ok(file) = file else { continue };
+                    let Ok(filename) = file.file_name().into_string() else { continue };
+                    let ext = filename.split('.').last().unwrap_or("<invalid>");
+                    if !system.file_extensions.contains(&ext.to_string()) {
+                        log_error(format!("Wrong file extension for file {}. Expected one of: {:?}, Got: {}", filename, &system.file_extensions, ext));
+                        continue;
+                    }
+                    let fancy_name = filename.split('.').next().unwrap().to_string();
+                    let mut args = emulator.args.clone();
+                    args.push(file.path().into_os_string().into_string().unwrap_or("".to_string()));
+
+                    let entry = MenuEntry {
+                        name: fancy_name,
+                        category: Category::Tools, // ignored
+                        uses_wayland: true,
+                        executable: emulator.executable.clone(),
+                        args,
+                        env: emulator.env.clone(),
+                    };
+                    system_tab.push((entry, id));
+                    id += 1;
+                }
+                roms.push((system.name.clone(), system_tab));
+            } else {
+                log_error(format!("{}'s rom directory, {}, does not exist, skipping ", &system.name, system.rom_directory.display()));
             }
         }
 
         let layout = Layout::builder();
         let mut tools_tab = layout.tab("System Tools");
-        for (name, id) in tools {
-            tools_tab = tools_tab.line().button_stateless(name, id as u128).endl();
+        for (entry, id) in tools {
+            tools_tab = tools_tab.line().button_stateless(&entry.name, id).endl();
+            entry_map.insert(id, entry);
         }
+        let layout = tools_tab.end_tab();
 
-        let mut programs_tab = tools_tab.tab("Programs");
-        for (name, id) in programs {
-            programs_tab = programs_tab.line().button_stateless(name, id as u128).endl();
+        let mut programs_tab = layout.tab("Programs");
+        for (entry, id) in programs {
+            programs_tab = programs_tab.line().button_stateless(&entry.name, id).endl();
+            entry_map.insert(id, entry);
+        }
+        let mut layout = programs_tab.end_tab();
+
+        for (name, romtab) in roms {
+            let mut tab = layout.tab(&name);
+            for (entry, id) in romtab {
+                tab = tab.line().button_stateless(&entry.name, id).endl();
+                entry_map.insert(id, entry);
+            }
+            layout = tab.end_tab();
         }
         
-        programs_tab.build()
+        (entry_map, layout.build())
     }
 }
 
@@ -171,58 +251,27 @@ fn run_entry(e: &MenuEntry) -> Result<()> {
     Ok(())
 }
 
-fn save_config(l: &MenuLayout) {
-    let conf = toml::to_string(l).expect("Failed to serialize config");
-    eprintln!("{}", conf);
+fn save_config(l: &MenuLayout, p: &Path) -> Result<()> {
+    let conf = toml::to_string(l).context("Failed to serialize config")?;
+    let mut conf_file = File::create(p).context("Failed to create/open config file")?;
+    conf_file.write_all(conf.as_bytes()).context("Failed to save config")?;
+    Ok(())
+}
+
+fn load_config(p: &Path) -> Result<MenuLayout> {
+    let mut conf_file = File::open(p).context("Failed to open config file")?;
+    let mut buf = String::new();
+    conf_file.read_to_string(&mut buf).context("Failed to read config file")?;
+    let config = toml::from_str(&buf).context("Failed to deserialize config file")?;
+    Ok(config)
 }
 
 fn main() {
-    let menu_layout = MenuLayout {
-        items: vec![
-            MenuEntry {
-                name: "Toggle SSH".to_string(),
-                category: Category::Tools,
-                uses_wayland: false,
-                executable: PathBuf::from("/smenu/toggle_ssh"),
-                args: vec![],
-                env: vec![],
-            },
-            MenuEntry {
-                name: "Power Off".to_string(),
-                category: Category::Tools,
-                uses_wayland: false,
-                executable: PathBuf::from("/sbin/poweroff"),
-                args: vec![],
-                env: vec![],
-            },
-            MenuEntry {
-                name: "Htop".to_string(),
-                category: Category::Tools,
-                uses_wayland: false,
-                executable: PathBuf::from("/usr/bin/htop"),
-                args: vec![],
-                env: vec![],
-            },
-            MenuEntry {
-                name: "Open shell".to_string(),
-                category: Category::Tools,
-                uses_wayland: false,
-                executable: PathBuf::from("/usr/bin/ash"),
-                args: vec![],
-                env: vec![],
-            },
-            MenuEntry {
-                name: "Launch kmscube".to_string(),
-                category: Category::Tools,
-                uses_wayland: false,
-                executable: PathBuf::from("/usr/bin/kmscube"),
-                args: vec![],
-                env: vec![],
-            },
-        ],
-    };
+    let config_path = PathBuf::from("./smenu.toml");
+    log_debug(format!("Loading config from {}", config_path.display()));
+    let menu_layout = load_config(&config_path).unwrap();
 
-    let layout = menu_layout.mk_sgui_layout();
+    let (entries, layout) = menu_layout.mk_sgui_layout();
     log_debug("Smenu starting up");
     let mut gui = Gui::new(layout);
     let state = loop {
@@ -233,7 +282,7 @@ fn main() {
                 break state;
             },
             GuiEvent::StatelessButtonPress(_, id) => {
-                if let Some(entry) = menu_layout.items.get(id as usize) {
+                if let Some(entry) = entries.get(&id) {
                     gui.set_ignore_hid(true);
                     thread::scope(|s| {
                         let h = s.spawn(move || {if let Err(e) = run_entry(&entry) {
@@ -242,7 +291,6 @@ fn main() {
                         while !h.is_finished() {
                             let _ = gui.get_ev();
                         }
-
                     });
                     gui.set_ignore_hid(false);
                 }
@@ -251,6 +299,8 @@ fn main() {
         }
     };
 
-    save_config(&menu_layout);
-    println!("{:#?}", state);
+    log_debug(format!("Saving config into {}", config_path.display()));
+    //if let Err(e) = save_config(&menu_layout, &config_path) {
+    //    log_rust_error(&*e, "Failed to save menu config", LogPriority::Critical);
+    //}
 }
